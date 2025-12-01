59165309db92e42f60f8b3d373be06fcf4b620ec
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-12-01 08:42:32 -0300

    feat: config package

--- devbook/api/src/config/config.go
+++ devbook/api/src/config/config.go
@@ -1,5 +1,34 @@
 package config
 
-func Load() {
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
+)
+
+var (
+	ConnectionString = ""
+	Port             = 0
+)
+
+func Load() {
+	var erro error
+
+	if erro = godotenv.Load(); erro != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	Port, erro = strconv.Atoi(os.Getenv("API_PORT"))
+	if erro != nil {
+		Port = 9000
+	}
+
+	ConnectionString = fmt.Sprintf("%s:%s@/%s?charset=utf8&parseTime=True&loc=Local",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
 }
